97ef8212ebc6790614949ae9326e619e068b9632
Abdelrahman Omran <me@omranic.com>
2017-07-20 18:06:37 +0200

    Tweak code consistency

--- src/EloquentBuilder.php
+++ src/EloquentBuilder.php
@@ -23,29 +23,27 @@ class EloquentBuilder extends Builder
         $builder = $this->applyScopes();
 
         $closure = function () use ($builder, $columns) {
-            $models = $builder->getModels($columns);
-
-            // if we actually found models we will also eager load any relationships that
+            // If we actually found models we will also eager load any relationships that
             // have been specified as needing to be eager loaded, which will solve the
             // n+1 query issue for the developers to avoid running a lot of queries.
-            if (count($models) > 0) {
+            if (count($models = $builder->getModels($columns)) > 0) {
                 $models = $builder->eagerLoadRelations($models);
             }
 
-            return $builder->getModel()->newcollection($models);
+            return $builder->model->newCollection($models);
         };
 
         // Check if cache is enabled
-        if ($builder->getModel()->getCacheLifetime()) {
-            return $builder->getModel()->cacheQuery($builder, $columns, $closure);
+        if ($builder->model->getCacheLifetime()) {
+            return $builder->model->cacheQuery($builder, $columns, $closure);
         }
 
         // Cache disabled, just execute query & return result
-        $result = call_user_func($closure);
+        $results = call_user_func($closure);
 
         // We're done, let's clean up!
-        $builder->getModel()->resetCacheConfig();
+        $builder->model->resetCacheConfig();
 
-        return $result;
+        return $results;
     }
 }
